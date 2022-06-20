2fe402a517640b6a9f6bed44065ee74f5ed9ee7c
Juan Sebastian <jsfc2199@gmail.com>
2022-06-20 13:39:50 -0500

    Project started, first commit

---
+++ src/main/java/com/jsfc/streamprogrammer/TwitchOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.jsfc.streamprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsfc/streamprogrammer/TwitchOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsfc.streamprogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
